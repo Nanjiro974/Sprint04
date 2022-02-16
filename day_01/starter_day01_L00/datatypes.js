24870988afabb4c37f4e5df9d55158c40e0342c3
Nanjiro974 <armougom.t@gmail.com>
2022-02-16 12:52:15 +0400

    First commit

---
+++ day_01/starter_day01_L00/datatypes.js
@@ -0,0 +1,44 @@
+console.log("chaine", typeof chaine);
+console.log("bool", typeof bool);
+console.log("undef", typeof undef);
+console.log("rien", typeof rien);
+
+let v0;
+let v1;
+let v2;
+let v3;
+
+let w0 = "val0";
+let w1 = "val1";
+let w2 = "val2";
+let w3 = "val3";
+
+var prenom = "Thomas";
+var nom = "ARMOUGOM";
+var status = "célibataire";
+var pays = "FRANCE";
+var age = "34";
+
+var prenom = "Thomas",
+  nom = "ARMOUGOM",
+  status = "célibataire",
+  pays = "FRANCE",
+  age = "34";
+
+let myAge;
+let yourAge;
+
+myAge = 26;
+yourAge = 30;
+
+console.log("J'ai", myAge, "ans.");
+console.log("Vous avez", yourAge, "ans.");
+
+let defi = "Pratiquer les algorithmes en JavaScript";
+console.log(defi);
+console.log(defi.length);
+console.log(defi.toUpperCase());
+console.log(defi.toLowerCase());
+
+console.log(defi.substr(0, 8));
+console.log(defi.substring(0, 8));
--- day_01/starter_day01_L00/variable.js
+++ day_01/starter_day01_L00/variable.js
@@ -1,18 +1,19 @@
 // *****Exercices: Data Types --> Level00 [4]*****
 
+// les commentaires peuvent rendre le code lisible
+// Bienvenue dans JavaScript
+/*
+les commentaires peuvent rendre le code lisible, facile à réutiliser
+et informatif
+*/
+
+let chaine = "toto";
+let bool = true;
+let undef;
+let rien = null;
 
 // Declaring different variables of different data types
 
-
-
-
 // Declaring variables with number values
 
-
-
-
-
-
 // Variables can also be declaring in one line separated by comma
-
-
