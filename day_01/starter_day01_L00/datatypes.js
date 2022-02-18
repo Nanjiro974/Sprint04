876eb2963ce9ca4942407692d352b36abee332ca
Nanjiro974 <armougom.t@gmail.com>
2022-02-18 15:52:48 +0400

    maj

--- day_01/starter_day01_L00/datatypes.js
+++ day_01/starter_day01_L00/datatypes.js
@@ -84,4 +84,20 @@ console.log(
   "verifie si la chaine de caracteres se termine par la chaine de caracteres fournie en argument"
 );
 const regex = /["a"]/g;
-console.log(text.match(regex));
+console.log(
+  text.match(regex),
+  "trouver tous les a dans la chaine de caractere"
+);
+//-----------------------------------------------------------------------------------------------------------------
+console.log("10 Jours en ".concat("JavaScript"));
+console.log(text.repeat(2));
+
+//---------------------------------------------------//////--------------------------------------------------------------//
+
+let declaration =
+  "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+console.log(declaration);
+let citation =
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+console.log(citation);
+console.log();
